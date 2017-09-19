fd8f0c9e22c779750a6228aa0ee5ee8e2f73ac5c
Betty <b.lam4392@gmail.com>
2017-09-19 14:47:47 -0700

    Complete User authenication backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :loggedIn?
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user || User.find_by_session_token(session[:session_token])
+  end
+
+  def signin!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def signout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def loggedIn?
+    !!current_user
+  end
+
+  # def require_signin
+  #   unless loggedIn? redirect_to
+  # end
 end
