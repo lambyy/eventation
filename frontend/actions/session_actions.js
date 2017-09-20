82e159c7026a52be8bd28b24b46a5fe01b496cb1
Betty <b.lam4392@gmail.com>
2017-09-19 17:17:52 -0700

    Complete User authenication Redux loop

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,39 @@
+import { postUser, postSession, deleteSession } from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+const receiveCurrentUser = (currentUser) => {
+  if (currentUser) {
+    const { id, first_name, last_name } = currentUser;
+    currentUser = { id, first_name, last_name };
+  }
+
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signup = (formUser) => dispatch => (
+  postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const login = (formUser) => dispatch => (
+  postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const logout = () => dispatch => (
+  deleteSession()
+    .then(user => dispatch(receiveCurrentUser(null)))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
--- frontend/eventation.jsx
+++ frontend/eventation.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import { signup, login, logout } from './actions/session_actions';
+window.signup = signup;
+window.login = login;
+window.logout = logout;
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById("root");
   ReactDOM.render(<h1>MY AMAZING PROJECT!</h1>, root);
 });
