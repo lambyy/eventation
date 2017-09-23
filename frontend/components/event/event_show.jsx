f0ba77fe8de6b26606ef79ee16010e96551771e0
Betty <b.lam4392@gmail.com>
2017-09-23 16:36:22 -0700

    Basic styling and layout for event show page

--- frontend/components/event/event_show.jsx
+++ frontend/components/event/event_show.jsx
@@ -5,10 +5,34 @@ class EventShow extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    if (!this.props.event) {
+      this.props.requestEvent(this.props.eventId);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.eventId !== nextProps.eventId) {
+      this.props.requestEvent(nextProps.eventId);
+    }
+  }
+
   render() {
+    console.log(this.props.event);
+    const { event } = this.props;
+
+    if (!event) return null;
 
     return (
-      <h1>EVENT SHOW PAGE</h1>
+      <div className="event-show">
+        <div className="event-image">
+          <img src={event.image_url}/>
+        </div>
+        <div className="event-digest"></div>
+        <div className="event-register"></div>
+        <div className="event-description">DESCRIPTION</div>
+        <div className="event-location">LOCATION</div>
+      </div>
     );
   }
 }
--- frontend/components/event/event_show_container.js
+++ frontend/components/event/event_show_container.js
@@ -3,12 +3,13 @@ import { requestEvent, removeEvent } from '../../actions/event_actions';
 import { clearErrors } from '../../actions/error_actions';
 import EventShow from './event_show';
 
-const mapStateToProps = (state) => ({
-
+const mapStateToProps = (state, {match}) => ({
+  event: state.entities.events[match.params.eventId],
+  eventId: match.params.eventId
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  requestEvent: (id) => dispatch(requestEvent(id))
 });
 
 export default connect(
