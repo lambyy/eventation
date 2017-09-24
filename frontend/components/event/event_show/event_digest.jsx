76bee4717c2298ec40ed211a7ce07165536773b9
Betty <b.lam4392@gmail.com>
2017-09-23 22:13:57 -0700

    Styled event digest on event show page

--- frontend/components/event/event_show/event_digest.jsx
+++ frontend/components/event/event_show/event_digest.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const EventDigest = ({ event, startDate }) => {
   return (
     <div className="event-digest">
-      <p>{startDate.toUTCString().slice(8, 12)}</p>
-      <p>{startDate.getUTCDate()}</p>
+      <div className="digest-date">
+        <p>{startDate.toUTCString().slice(8, 12)}</p>
+        <p>{startDate.getUTCDate()}</p>
+      </div>
       <p>{event.title}</p>
     </div>
   );
