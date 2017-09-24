1fd23022bb66c73a59ac9917467352786c50438b
Betty <b.lam4392@gmail.com>
2017-09-23 23:10:04 -0700

    Completed styling for event show page

--- frontend/components/event/event_show/event_description.jsx
+++ frontend/components/event/event_show/event_description.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const EventDescription = ({ description }) => {
+  const paragraphs = description.split("\n");
+  const text = paragraphs.map( (paragraph, idx) => (
+    <p key={idx}>{paragraph}</p>
+  ));
+
   return (
     <div className="event-description">
       <h3>DESCRIPTION</h3>
-      <p>{description}</p>
+      <div>{text}</div>
     </div>
   );
 };
