e4dffe6bc26b819c9b0452967cf29a1d9f52f8ed
Betty <b.lam4392@gmail.com>
2017-09-24 16:51:42 -0700

    Refactored detail form from event form

--- frontend/components/event/event_form.jsx
+++ frontend/components/event/event_form.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
-const _categories = "Music Arts Food&Drink Other".split(" ");
-const _eventTypes = "Class Concert Performance Other".split(" ");
+import DetailForm from './event_form/detail_form';
 
 class EventForm extends React.Component {
   constructor(props){
@@ -18,11 +16,6 @@ class EventForm extends React.Component {
     this.defaultState = this.defaultState.bind(this);
 
     this.renderEventErrors = this.renderEventErrors.bind(this);
-
-    this.renderDetailForm = this.renderDetailForm.bind(this);
-    this.renderEventTypeOptions = this.renderEventTypeOptions.bind(this);
-    this.renderCategoryOptions = this.renderCategoryOptions.bind(this);
-
   }
 
   componentWillMount() {
@@ -83,99 +76,6 @@ class EventForm extends React.Component {
     return this.props.errors.map((error, idx) => <li key={idx}>{error}</li>);
   }
 
-  renderDetailForm() {
-    const { title, location, image_url, description, start_date,
-            end_date, event_type, category } = this.state;
-
-    return (
-      <div className="detail-form">
-        <label>
-          EVENT TITLE
-          <br/>
-          <input type="text" value={title}
-              onChange={this.update("title")}
-              placeholder="Give it a short distinct name"/>
-        </label>
-        <br/>
-        <label>
-          LOCATION
-          <br/>
-          <input type="text" value={location}
-              onChange={this.update("location")}
-              placeholder="Specify where event will be"/>
-        </label>
-
-          <div className="event-time">
-            <label>
-              STARTS
-              <br/>
-              <input type="datetime-local" value={start_date.slice(0, 16)}
-                onChange={this.update("start_date")}/>
-            </label>
-            <br/>
-            <label>
-              ENDS
-              <br/>
-              <input type="datetime-local" value={end_date.slice(0, 16)}
-                onChange={this.update("end_date")}/>
-            </label>
-          </div>
-
-        <label>
-          EVENT IMAGE
-          <br/>
-          <input type="text" value={image_url}
-              onChange={this.update("image_url")}
-              placeholder="Choose an image that brings your event to life"/>
-        </label>
-        <br/>
-        <label>
-          EVENT DESCRIPTION
-          <br/>
-          <textarea value={description}
-              onChange={this.update("description")}></textarea>
-        </label>
-        <br/>
-        <label>
-          EVENT TYPE
-          <br/>
-          <select value={event_type} onChange={this.update("event_type")}>
-            <option value="1" disabled>Select the type of event</option>
-            {this.renderEventTypeOptions()}
-          </select>
-        </label>
-        <br/>
-        <label>
-          EVENT TOPIC
-          <br/>
-          <select value={category} onChange={this.update("category")}>
-            <option value="1" disabled>Select a topic</option>
-            {this.renderCategoryOptions()}
-          </select>
-        </label>
-      </div>
-    );
-  }
-
-  renderEventTypeOptions() {
-    const eventTypeOptions = _eventTypes.map( (eventType, idx) => {
-      return (
-        <option key={idx} value={eventType}>{eventType}</option>
-      );
-    });
-    return eventTypeOptions;
-  }
-
-  renderCategoryOptions() {
-    const categoryOptions = _categories.map( (category, idx) => {
-      const optionText = category.split("&").join(" & ");
-      return (
-        <option key={idx} value={category}>{optionText}</option>
-      );
-    });
-    return categoryOptions;
-  }
-
   render() {
     return (
       <div>
@@ -188,7 +88,7 @@ class EventForm extends React.Component {
         <form className="event-form">
           <p><span>1</span>Event Details</p>
           <ul className="error-display">{this.renderEventErrors()}</ul>
-          {this.renderDetailForm()}
+          <DetailForm update={this.update} state={this.state}/>
           <p><span>2</span>Create Tickets</p>
           <br/>
           <input type="submit" className="green-button"
---
+++ frontend/components/event/event_form/detail_form.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+const _categories = "Music Arts Food&Drink Other".split(" ");
+const _eventTypes = "Class Concert Performance Other".split(" ");
+
+class DetailForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderEventTypeOptions() {
+    const eventTypeOptions = _eventTypes.map( (eventType, idx) => {
+      return (
+        <option key={idx} value={eventType}>{eventType}</option>
+      );
+    });
+    return eventTypeOptions;
+  }
+
+  renderCategoryOptions() {
+    const categoryOptions = _categories.map( (category, idx) => {
+      const optionText = category.split("&").join(" & ");
+      return (
+        <option key={idx} value={category}>{optionText}</option>
+      );
+    });
+    return categoryOptions;
+  }
+
+  render() {
+    const { title, location, image_url, description, start_date,
+            end_date, event_type, category } = this.props.state;
+    const { update } = this.props;
+
+    return (
+      <div className="detail-form">
+        <label>
+          EVENT TITLE
+          <br/>
+          <input type="text" value={title}
+              onChange={update("title")}
+              placeholder="Give it a short distinct name"/>
+        </label>
+        <br/>
+        <label>
+          LOCATION
+          <br/>
+          <input type="text" value={location}
+              onChange={update("location")}
+              placeholder="Specify where event will be"/>
+        </label>
+
+          <div className="event-time">
+            <label>
+              STARTS
+              <br/>
+              <input type="datetime-local" value={start_date.slice(0, 16)}
+                onChange={update("start_date")}/>
+            </label>
+            <br/>
+            <label>
+              ENDS
+              <br/>
+              <input type="datetime-local" value={end_date.slice(0, 16)}
+                onChange={update("end_date")}/>
+            </label>
+          </div>
+
+        <label>
+          EVENT IMAGE
+          <br/>
+          <input type="text" value={image_url}
+              onChange={update("image_url")}
+              placeholder="Choose an image that brings your event to life"/>
+        </label>
+        <br/>
+        <label>
+          EVENT DESCRIPTION
+          <br/>
+          <textarea value={description}
+              onChange={update("description")}></textarea>
+        </label>
+        <br/>
+        <label>
+          EVENT TYPE
+          <br/>
+          <select value={event_type} onChange={update("event_type")}>
+            <option value="1" disabled>Select the type of event</option>
+            {this.renderEventTypeOptions()}
+          </select>
+        </label>
+        <br/>
+        <label>
+          EVENT TOPIC
+          <br/>
+          <select value={category} onChange={update("category")}>
+            <option value="1" disabled>Select a topic</option>
+            {this.renderCategoryOptions()}
+          </select>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default DetailForm;
