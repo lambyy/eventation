41c7d7553fa2c2631d324b247f89d829a094ce59
Betty <b.lam4392@gmail.com>
2017-09-25 22:40:29 -0700

    Created registration action creator and thunk action creators

--- frontend/actions/registration_actions.js
+++ frontend/actions/registration_actions.js
@@ -3,9 +3,22 @@ import { postRegistration, deleteRegistration } from '../util/registration_util'
 export const RECEIVE_REGISTRATION = "RECEIVE_REGISTRATION";
 export const RECEIVE_REMOVE_REGISTRATION = "RECEIVE_REMOVE_REGISTRATION";
 
-  const receiveRegistration = (registration) => ({
-    type: RECEIVE_REGISTRATION,
-    registration
-  });
+const receiveRegistration = (registration) => ({
+  type: RECEIVE_REGISTRATION,
+  registration
+});
 
-  // const receiveRemoveRegistration = ()
+const receiveRemoveRegistration = (registration) => ({
+  type: RECEIVE_REMOVE_REGISTRATION,
+  registration
+});
+
+export const createRegistration = (formRegistration) => dispatch => {
+  postRegistration(formRegistration)
+    .then(registration => dispatch(receiveRegistration(registration)));
+};
+
+export const removeRegistration = (id) => dispatch => {
+  deleteRegistration(id)
+    .then(registration => dispatch(receiveRemoveRegistration(registration)));
+}
