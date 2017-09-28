519c53943fb873ccd8606268cd356bc4baf3572b
Betty <b.lam4392@gmail.com>
2017-09-27 19:01:54 -0700

    Created user profile container and component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,6 +4,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBar from './navbar/navbar';
 import Dashboard from './dashboard';
 import SessionFormContainer from './session/session_form_container';
+import ProfileContainer from './profile/profile_container';
 import EventIndexContainer from './event/event_index_container';
 import EventFormContainer from './event/event_form_container';
 import EventShowContainer from './event/event_show_container';
@@ -15,6 +16,7 @@ const App = () => (
       <Route exact path="/" component={Dashboard} />
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/signup" component={SessionFormContainer} />
+      <ProtectedRoute path="/profile/:filter" component={ProfileContainer} />
       <Route path="/events/browse" component={EventIndexContainer} />
       <ProtectedRoute path="/events/create" component={EventFormContainer} />
       <Route exact path="/events/:eventId" component={EventShowContainer} />
---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>
+        USER PROFILE!
+      </div>
+    );
+  }
+
+}
+
+export default Profile;
